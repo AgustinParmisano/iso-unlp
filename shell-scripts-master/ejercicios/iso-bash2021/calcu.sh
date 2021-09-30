570d7f0db34030c84052da8329057c9934996024
Agustin Parmisano <agustinparmisano@gmail.com>
2021-09-30 20:53:53 -0300

    Agregando ejercicios de break continue, login de users y calcu

--- shell-scripts-master/ejercicios/iso-bash2021/calcu.sh
+++ shell-scripts-master/ejercicios/iso-bash2021/calcu.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
+if [ $# -ne 3 ]; then
+	echo "Se deben enviar 3 parametros: valor1 operando valor2"
+	echo "Los operandos son: + - / \*"
+	echo "Ejemplo: 2 + 6"
+	echo "Ejemplo2: 4 \* 2"
+	exit 1
+fi
 
+operando=$2
 
-'*') commands;;
+case $operando in
+	'+') 
+		echo $(expr $1 + $3)
+		;;
+	'-') 
+		echo $(expr $1 - $3)
+		;;
+	'*') 
+		echo $(expr $1 \* $3)
+		;;
+	'/') 
+		echo $(expr $1 / $3)
+		;;
+	*)
+		echo "No existe el operando $operando"
+		echo "Los operandos son: + - / \*"
+		exit 2
+		;;
+esac
 
-
-function suma {
-	echo $1 + $2
-}
-
-
+exit 0
--- shell-scripts-master/ejercicios/iso-bash2021/usuarix_login.sh
+++ shell-scripts-master/ejercicios/iso-bash2021/usuarix_login.sh
@@ -5,16 +5,22 @@
 #el nombre del usuario será pasado por parámetro). Cuando el usuario finalmente se loguee,
 #el programa deberá mostrar el mensaje ”Usuario XXX logueado en el sistema” y salir.
 
-usu=$1
-secs=2
+#Para probar el login de un user hay que entrar en otra tty con ctrl + alt + f2 o f3 .. f7 y logearnos con el otro user
+#Para volver a la tty :0 que es la interfaz gráfica es con crtl + alt f1 o f7
 
+# Checkeo de parametros
 if [ $# -ne 1 ]; then
 	echo "Se debe enviar 1 usuarix"
 	exit 1
 fi
 
+usu=$1
+secs=2
+
 while true; do
 	cantidad_usu=$(who | grep $usu | cut -d" " -f1 | wc -l)
+	#lo mismo pero con awk para ir practicandolo
+	#cantidad_usu=$(who | grep $usu | awk -F" " '{ print $1 }' | wc -l)
 	if [ $cantidad_usu -eq 0 ]; then
 		echo "Todavia no entro $usu. Esperamos $secs segundos"
 		sleep $secs
